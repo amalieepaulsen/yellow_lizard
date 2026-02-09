73fa22d95ec71e7bf2354c01b6e59245695d2685
amalieepaulsen <my.nusse@hotmail.no>
2026-02-09 13:34:23 +0100

    Created figure with body mass per species.

--- yellow_lizard_penguins.R
+++ yellow_lizard_penguins.R
@@ -5,7 +5,6 @@
 ## Purpose of the script: GitHub testing
 ##
 ## Author: Amalie Elisabeth Paulsen
-##
 ## Date created: 2026-02-09
 ##
 ## -----------------------------------------------------------------------------
@@ -18,11 +17,25 @@
 library(palmerpenguins)
 library(tidyverse)
 
+# Custom settings
+set_theme(theme_bw())
+
 ## -----------------------------------------------------------------------------
 
 ## Average penguin body mass by species
 species_bm <- penguins |>
   drop_na() |>
-  summarize(mean = mean(body_mass_g), .by = species)
+  select(species, body_mass_g)
+
+## -----------------------------------------------------------------------------
+
+## Body mass box plot
+species_bm |>
+  ggplot(aes(x = species, y = body_mass_g, fill = species, alpha = 0.4)) +
+  geom_boxplot() +
+  theme(
+    legend.position = "none",
+    legend.title = element_blank()
+  )
 
 ## -----------------------------------------------------------------------------
